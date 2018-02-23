368a47d7b8a9d522b463e418db015e69e0d98303
Kennedy Rose <kennedy@kennedyrose.com>
2018-02-23 08:27:58 -0600

    Initial commit

---
+++ src/html.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react'
+import * as PropTypes from 'prop-types'
+
+const options = {
+	lang: 'en',
+	dir: 'ltr', // https://www.w3.org/TR/html4/struct/dirlang.html#blocklevel-bidi
+	bgColor: '#FFFFFF',
+	width: 600,
+}
+
+const propTypes = {
+	headComponents: PropTypes.node.isRequired,
+	body: PropTypes.node.isRequired,
+	postBodyComponents: PropTypes.node.isRequired,
+}
+
+class Html extends Component {
+	render() {
+		return (
+			<html
+				lang={options.lang}
+				dir={options.dir}
+				xmlns='http://www.w3.org/1999/xhtml'>
+				<head>
+					{this.props.headComponents}
+					<meta http-equiv='Content-Type' content='text/html; charset=utf-8' />
+					<meta httpEquiv='X-UA-Compatible' content='IE=edge' />
+					<meta name='viewport' content='width=device-width' />
+				</head>
+				<body bgcolor={options.bgColor} width='100%' style={{
+						'-webkit-font-smoothing': 'antialiased',
+						width: '100% !important',
+						background: options.bgColor,
+						'-webkit-text-size-adjust': 'none',
+						margin: 0,
+						padding: 0,
+						minWidth: '100%',
+						direction: options.dir,
+					}}>
+					<table bgcolor={options.bgColor} id='__bodyTable__' width='100%' style={{
+							'-webkit-font-smoothing': 'antialiased',
+							width: `${options.width}px !important`,
+							background: options.bgColor,
+							'-webkit-text-size-adjust': 'none',
+							margin: '0 auto',
+							padding: 0,
+						}}>
+						<tr>
+							<td align='center' id='___gatsby' dangerouslySetInnerHTML={{ __html: this.props.body }} />
+						</tr>
+					</table>
+
+					{this.props.postBodyComponents}
+				</body>
+			</html>
+		)
+	}
+}
+
+Html.propTypes = propTypes
+
+module.exports = Html
---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Block from '../components/block'
+
+export default class Index extends React.Component {
+	render() {
+		return (
+			<Block padding='0 20'>
+				This is a test
+			</Block>
+		)
+	}
+}
